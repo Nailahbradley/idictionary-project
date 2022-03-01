161447892e41ce9843de7ffc6fa362aafe9eafd5
Nailahbradley <bradleynailah@gmail.com>
2022-02-28 19:30:20 -0600

    Implemented logo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.jpg";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} alt="logo" className="img-fluid" />
+    </div>
+  );
+}

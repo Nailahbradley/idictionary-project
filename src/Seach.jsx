acbc998d44519cc700bf06fd1f8a45a92388d009
Nailahbradley <bradleynailah@gmail.com>
2022-03-03 19:36:41 -0600

    Added axios and Api url

--- src/Seach.jsx
+++ src/Seach.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { BsSearch } from "react-icons/bs";
 
 import "./search.css";
 
 export default function Search() {
   let [keyword, setkeyword] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function updateKeyword(event) {
     setkeyword(event.target.value);
   }

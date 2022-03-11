05d729b7287d4eb0f2175ae1105ff9e3e2b4ffa1
Nailahbradley <bradleynailah@gmail.com>
2022-03-10 18:50:25 -0600

    debug

--- src/Phonetics.jsx
+++ src/Phonetics.jsx
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
   if (props.phonetic) {
     return (
       <div className="Phonetics">
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
           Listen
         </a>
         {props.phonetic.text}
